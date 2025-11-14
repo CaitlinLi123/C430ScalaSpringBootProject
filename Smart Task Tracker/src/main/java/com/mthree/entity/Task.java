33d17fe9de5fc717903794a1abdf78410d8c4ef3
sasha-sokol <sasha.sokolniki@gmail.com>
2025-11-14 12:18:31 +0000

    resolved conflicts wrong oops

--- Smart Task Tracker/src/main/java/com/mthree/entity/Task.java
+++ Smart Task Tracker/src/main/java/com/mthree/entity/Task.java
@@ -1,8 +1,5 @@
 package com.mthree.entity;
 
-import javax.annotation.Priority;
-import javax.persistence.*;
-import javax.validation.constraints.NotNull;
 import java.time.LocalDate;
 
 import javax.annotation.Priority;
