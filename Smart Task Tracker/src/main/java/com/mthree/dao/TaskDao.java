8ba542b61ce4a89b9a0787080c33f23f92dc0cdd
Kabinan Balakrishnan <balakrishnankabinan@gmail.com>
2025-11-14 12:04:12 +0000

    Minor change in TaskDao.java interface

--- Smart Task Tracker/src/main/java/com/mthree/dao/TaskDao.java
+++ Smart Task Tracker/src/main/java/com/mthree/dao/TaskDao.java
@@ -1,11 +1,15 @@
 package com.mthree.dao;
 
+import com.mthree.entity.Category;
 import com.mthree.entity.Task;
 import org.springframework.data.jpa.repository.JpaRepository;
 import org.springframework.stereotype.Repository;
 
+import java.util.List;
+
 @Repository
 public interface TaskDao extends JpaRepository<Task, Integer> {
+    List<Task> findByCategory(Category category);
 }
 
 
