aa0351912902f8dd93dde56e0838037b3081a3f2
Kabinan Balakrishnan <balakrishnankabinan@gmail.com>
2025-11-14 12:14:44 +0000

    Added spring application

--- Smart Task Tracker/src/main/java/com/mthree/App.java
+++ Smart Task Tracker/src/main/java/com/mthree/App.java
@@ -1,7 +1,12 @@
 package com.mthree;
 
+import org.springframework.boot.SpringApplication;
+
 public class App {
+
+
     public static void main(String[] args) {
+        SpringApplication.run(App.class, args);
 
     }
 }
