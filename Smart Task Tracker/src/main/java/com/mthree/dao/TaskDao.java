190c761a404807673f6b15e5f88c173c5a644206
sasha-sokol <sasha.sokolniki@gmail.com>
2025-11-14 13:26:11 +0000

    change task dao to use long

--- Smart Task Tracker/src/main/java/com/mthree/dao/TaskDao.java
+++ Smart Task Tracker/src/main/java/com/mthree/dao/TaskDao.java
@@ -1,14 +1,15 @@
 package com.mthree.dao;
 
-import com.mthree.entity.Category;
-import com.mthree.entity.Task;
+import java.util.List;
+
 import org.springframework.data.jpa.repository.JpaRepository;
 import org.springframework.stereotype.Repository;
 
-import java.util.List;
+import com.mthree.entity.Category;
+import com.mthree.entity.Task;
 
 @Repository
-public interface TaskDao extends JpaRepository<Task, Integer> {
+public interface TaskDao extends JpaRepository<Task, Long> {
 
     /*
     *List tasks given category
