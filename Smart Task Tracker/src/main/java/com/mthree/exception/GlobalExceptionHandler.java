2660d933662de19a27d4033c1eaaff4f31bd5d2e
Yu Li <104264689+CaitlinLi123@users.noreply.github.com>
2025-11-14 11:21:48 +0000

    feat: add the skeleton of exception handling

---
+++ Smart Task Tracker/src/main/java/com/mthree/exception/GlobalExceptionHandler.java
@@ -0,0 +1,20 @@
+import javax.servlet.http.HttpServletRequest;
+
+import org.springframework.http.HttpStatus;
+import org.springframework.http.ResponseEntity;
+import org.springframework.web.bind.annotation.ControllerAdvice;
+import org.springframework.web.bind.annotation.ExceptionHandler;
+
+@ControllerAdvice
+public class GlobalExceptionHandler {
+
+    // This will be uncommented when all other custom exception classes are handled
+    // in this file. Comment it for now to prevent hiding important information when
+    // exception happens.
+    // @ExceptionHandler(Exception.class)
+    // public ResponseEntity<String> handleGeneric(Exception e) {
+    // return
+    // ResponseEntity.status(HttpStatus.INTERNAL_SERVER_ERROR).body("Something went
+    // wrong. " + e.getMessage());
+    // }
+}
