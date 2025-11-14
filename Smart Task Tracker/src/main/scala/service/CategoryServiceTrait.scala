8d837c7413d4bbe820f3a8002cc46ac19551586a
Kabinan Balakrishnan <balakrishnankabinan@gmail.com>
2025-11-14 12:46:03 +0000

    Skeleton of CategoryServiceImpl

--- Smart Task Tracker/src/main/scala/service/CategoryServiceTrait.scala
+++ Smart Task Tracker/src/main/scala/service/CategoryServiceTrait.scala
@@ -1,5 +1,19 @@
 package service
 
+import com.mthree.entity.Category
+
+import java.util.Optional
+
 trait CategoryServiceTrait {
+
+  def createCategory(category: Category): Optional[Category]
+
+  def getAllCategories(): List[Category]
+
+  def getCategoryById(id: Long): Optional[Category]
+
+  def updateCategory(updated: Category): Optional[Category]
+
+  def deleteCategory(id: Long): Unit
 
 }
